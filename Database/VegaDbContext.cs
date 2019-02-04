42da92ac798aaca216df20b7b5512c809860ee3e
Pavel Vaverka <pvel.vaverka@centrum.cz>
2019-02-05 00:39:12 +0100

    db updated

--- Database/VegaDbContext.cs
+++ Database/VegaDbContext.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Database
 {
     public class VegaDbContext : DbContext
     {
-        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
-        {
+        public DbSet<Make> Make { get; set; }
+//        public DbSet<Model> Model { get; set; }
 
-        }
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)        { }
+
     }
 }
