fa2f2c8310a54173f2782fc39a25f39927036a18
Pavel Vaverka <pvel.vaverka@centrum.cz>
2019-02-04 23:11:11 +0100

    DbContext added

---
+++ Database/VegaDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace vega.Database
+{
+    public class VegaDbContext : DbContext
+    {
+        public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using vega.Database;
 
 namespace vega
 {
@@ -21,6 +23,8 @@ namespace vega
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
