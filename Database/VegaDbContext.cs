04834a9c0659fdfd158fcc98b1dedac8c763c69e
Pavel Vaverka <pvel.vaverka@centrum.cz>
2019-02-05 01:53:45 +0100

    Constraints

--- Database/VegaDbContext.cs
+++ Database/VegaDbContext.cs
@@ -5,7 +5,7 @@ namespace vega.Database
 {
     public class VegaDbContext : DbContext
     {
-        public DbSet<Make> Make { get; set; }
+        public DbSet<Make> Makes { get; set; }
 //        public DbSet<Model> Model { get; set; }
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options) : base(options)        { }
--- Models/Make.cs
+++ Models/Make.cs
@@ -1,18 +1,22 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
     public class Make
     {
-        public int Id{get;set;}
-        public string Name {get; set;}
+        public int Id { get; set; }
 
-        public ICollection<Model> Models {get;set;}
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public ICollection<Model> Models { get; set; }
 
         public Make()
         {
-            Models=new Collection<Model>();
+            Models = new Collection<Model>();
         }
     }
 }
